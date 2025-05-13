5922107d63168e3ba2ceb44505c3f719f9da13e3
Sunidhi Chaturvedi <sunidhichaturvedi258012@gmail.com>
2025-05-14 00:22:50 +0530

    Create Count Pairs whose sum is less than target.cpp

---
+++ Count Pairs whose sum is less than target.cpp
@@ -0,0 +1,20 @@
+// Given an array arr[] and an integer target. You have to find the number of pairs in the array whose sum is strictly less than the target.
+
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Your code here
+        sort(arr.begin(), arr.end());
+        int left = 0, right = arr.size() - 1;
+        int count = 0;
+        while (left < right) {
+            if (arr[left] + arr[right] < target) {
+                count += (right - left);  
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+};
