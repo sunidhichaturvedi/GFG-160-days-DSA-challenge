1bf6cdad2869868bf1a870d2d21d50963349a4df
Sunidhi Chaturvedi <sunidhichaturvedi258012@gmail.com>
2025-07-12 19:21:29 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ Longest Palindromic Subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalinSubseq(string &s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        // Every single character is a palindrome of length 1
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        // Fill the dp table
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j]) {
+                    dp[i][j] = 2 + dp[i+1][j-1];
+                } else {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[0][n-1];
+    }
+};
